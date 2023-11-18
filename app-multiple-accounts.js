d4f96c5e9cb40299a2f93241079aa14b7e11daae
yan043 <mahdian043@gmail.com>
2023-11-18 10:16:34 +0800

    test fix

--- app-multiple-accounts.js
+++ app-multiple-accounts.js
@@ -65,7 +65,7 @@ const createSession = function (id, description) {
   const client = new Client({
     restartOnAuthFail: true,
     puppeteer: {
-      headless: true,
+      headless: false,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
